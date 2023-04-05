4d8e0c736e114d203b4e693d9e707c9c11e96636
joao <mibfiap@outlook.com>
2023-04-05 16:30:24 -0300

    RM550849 questao 3 feita

---
+++ script.js
@@ -0,0 +1,71 @@
+//Questão 3
+/*
+let peso =prompt("Digite seu peso em kg: ")
+let altura =prompt("digite sua Altura em metros (x.xx): ")
+
+ imc = peso / (altura * altura);
+ 
+
+if (imc < 18.5){
+    document.write("Abaixo do peso.")
+}
+else if (imc >= 18.5 && imc <= 24.9){
+    document.write("Peso ideal.")
+}
+else{
+    document.write("Acima do peso.")
+}
+console.log(imc)
+*/
+
+
+
+
+
+//Questao 6
+
+/*
+var n1, n2, n3, n4, n5, n6, n7;
+
+n1 = parseFloat(prompt("Informe a primeira nota: "));
+n2 = parseFloat(prompt("Informe a segunda nota: "));
+n3 = parseFloat(prompt("Informe a terceira nota: "));
+n4 = parseFloat(prompt("Informe a quarta nota: "));
+n5 = parseFloat(prompt("Informe a quinta nota: "));
+n6 = parseFloat(prompt("Informe a sexta nota: "));
+n7 = parseFloat(prompt("Informe a sétima nota: "));
+
+media = (n1 + n2 + n3 + n4 + n5 + n6 + n7) / 7;
+
+console.log(media)
+
+if (media >= 6) {
+document.write("Aprovado");
+} else {
+document.write("Reprovado");
+}
+*/
+
+
+
+
+//Questão 7
+/*
+let nome = prompt("Digite seu Nome: ")
+let idade = prompt("Digite sua Idade: ")
+let curso = prompt("Digite seu Curso: ")
+let ano = prompt("Digite o ano que você ingressou: ")
+
+
+document.write("Seu nome é ", nome)
+document.write("<br>")
+document.write("Você tem ", idade, " anos de idade")
+document.write("<br>")
+document.write("Você cursa: ", curso)
+document.write("<br>")
+document.write("Você ingressou no ano de: ", ano)
+*/
+
+
+
+
